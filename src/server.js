3b01c6630de86bd2759e626d7593283240b66a29
RenNeedsSleep <mohammedsohailali2409@gmail.com>
2026-05-20 15:10:27 +0530

    I added rate limiting using redis, the upstash is an instance of the ratelimiter with specific limits

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/ratelimiter.js";
 
 dotenv.config();
 const app = express();
@@ -10,11 +11,20 @@ const PORT = process.env.PORT || 5001;
 // we are making sure that the port is also hidden, or else it will run on default port
 
 //dotenv helps us such that we can hide important variables such as our MONGO_URI
-connectDB();
 
 //middleware
-app.use(express.json())
+app.use(express.json()) //this express.json is a middle ware, which gets you the body of the model to access the title and content fields
+
+//middlewafre is a function that tuns in between of requests and response
+app.use(rateLimiter)
+app.use((req,res,next) => {
+     console.log("We just got a new request")
+     next()
+})
+
+// everytime you run this, you can see that before the request is fullfilled you can see this message
 /* Http responses
+
 
 1xx- Informational
 
@@ -29,6 +39,7 @@ app.use(express.json())
 
      404- Not found
      401- Bad Request
+     429 - too many requests
      
      anything that is the clients fault
      
@@ -41,9 +52,13 @@ app.use(express.json())
 
 app.use("/api/notes", notesRoutes)
 
+connectDB().then(() => {
+     app.listen(PORT, () => {
+     console.log("Port online at :", PORT); //first we initiate the database then we start it
+     //if the connection fails, whats the point of starting the database
+});
+})
+
 //we prefixed all the routes with this specific route prefix
 
-app.listen(PORT, () => {
-     console.log("Port online at :", PORT);
-});
 
